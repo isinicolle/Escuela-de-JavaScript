a861a2506e3adb8904e050d58e5e2580d1de5631
Isis Zapata <isis.zapata.hn@gmail.com>
2022-07-06 22:48:47 -0600

    Update 7.trueyfalse.js

--- Profundiza en JavaScript y añade interactividad con React/Curso Básico de JavaScript/7.trueyfalse.js
+++ Profundiza en JavaScript y añade interactividad con React/Curso Básico de JavaScript/7.trueyfalse.js
@@ -26,3 +26,18 @@ más
 if(InputEvent.value) {
    
 }
+
+//Ejemplos en los que Boolean devuelve Falso:
+Boolean(0); //false
+Boolean(null); //false
+Boolean(NaN); //false
+Boolean(undefined); //false
+Boolean(false); //false
+Boolean(""); //false
+
+//Ejemplos en los que Boolean devuelve verdadero:
+Boolean(1); //true para 1 o cualquier número diferente de cero (0)
+Boolean("a"); //true para cualquier caracter o espacio en blanco en el string
+Boolean([]); //true aunque el array esté vacío
+Boolean({}); //true aunque el objeto esté vacío
+Boolean(function(){}); //Cualquier función es verdadera también

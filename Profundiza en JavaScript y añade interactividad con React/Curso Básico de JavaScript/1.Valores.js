dd989e44bbdd3a9faf266e9548b4b824dcf98508
Isis Zapata <isis.zapata.hn@gmail.com>
2022-07-06 21:29:45 -0600

    Create 1.Valores.js

---
+++ Profundiza en JavaScript y añade interactividad con React/Curso Básico de JavaScript/1.Valores.js
@@ -0,0 +1,50 @@
+//Video: Elementos de un Lenguaje de Programación: Variables, Funciones y Sintaxis
+//https://platzi.com/clases/1814-basico-javascript/26292-elementos-de-un-lenguaje-de-programacion-variables/
+
+//ejercicio de valores
+
+// Tipos primitivos = valores basicos para generar codigo
+
+40   // Numeros 
+3.14
+
+ "Diego De Granda"  // Strings (cadenas de texto), Esto es la forma en la que puedo crear algo que se pueda imprimir en la pantalla, o escribir información, que el usuario pueda entender y leer. utilizar palabras, o frases.
+
+ 'Diego De Granda'
+
+ true   // Booleanos, estos fueron inventados por George Boole, un Matemático británico. y esto hace referencia a cosas que podamos validar.
+ false
+
+ null   // Valores vacios (Empty values), es como un placeholder (espacio reservado para algun valor) de algún valor faltante.
+ 
+ undefined // Valores indefinidos (Undefined values), es como un valor que no se ha definido.
+
+
+ /* ================================================================ */ 
+ 
+ // Tipos de objetos o no primitivos
+
+ [1, 2, 3, 4] // corchetes // arrays
+ { nombre: 'Diego' }  // llaves // objetos
+
+
+ //Tipos de datos
+Number 
+Boolean
+String
+undefined
+null
+Object
+Symbol
+
+// typeof es un tipo de operador unitario "Unary operator" que trabaja con 1 operando (variable)
+
+var nombre; 
+
+typeof 30 
+typeof true
+typeof "Diego" 
+typeof nombre 
+typeof null
+typeof {}
+typeof []

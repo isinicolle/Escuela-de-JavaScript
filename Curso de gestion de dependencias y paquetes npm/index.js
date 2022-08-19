6904ddb1981cd223665cca446e318b39d6b51a84
Isis Zapata <isis.zapata.hn@gmail.com>
2022-08-18 21:03:32 -0600

    Update index.js

--- Curso de gestion de dependencias y paquetes npm/index.js
+++ Curso de gestion de dependencias y paquetes npm/index.js
@@ -17,4 +17,27 @@
 
 //npm init iniciar el proyecto llenando la iformacion del proyecto
 
+//npm init -y o yes para que no pregunte mas al usuario e instale todo automaticamente
+
+//npm set init-author-name "Juan" 
+//npm set init-author-email "juan   @gmail.com"
+//npm set init-author-url "
+//npm set init-author-license "MIT"
+//npm set init-author-license-url "https://opensource.org/licenses/MIT"
+//asi guardamos configuracion por terminal
+
+//npm outdate -u actualiza los paquetes que estan desactualizados
+
+//garantizar que el proyecto este bien configurado y actualizado 
+
+//npm outdate --dd vemos como corre por detras
+
+//npm outdate vemos paquetes desactualziados
+//npm update actualiza los paquetes que estan desactualizados
+
+//npm install json-server@lastest actualiza la ultima version del paquete
+
+//qutiar un apquete npm uninstall quitar un paquete
+
+//npm unistall webpack --no-save quita el paquete webpack de la lista de paquetes instalados se quita de node pero nod e package.json
 

558027d23a272e8a71e6e2fc2b431c0db220a0f2
Isis Zapata <isis.zapata.hn@gmail.com>
2022-08-18 20:48:15 -0600

    .
    .

---
+++ Curso de gestion de dependencias y paquetes npm/index.js
@@ -0,0 +1,20 @@
+//--save es necesario para llevar a produccion
+
+//--save-dev solo es necesario en el entorno de trabajo no en prdouccion
+
+//-D dependencia de desarrollo
+
+//-S dependencia de produccion
+
+//-G dependencia de produccion y desarrollo osea global ocupa permisos de admin
+
+//nodemon escucha el servidor
+
+//npm list -g  --depth 0
+//veo la lista de los paquetes instalados
+
+//-O de opcional
+
+//npm init iniciar el proyecto llenando la iformacion del proyecto
+
+

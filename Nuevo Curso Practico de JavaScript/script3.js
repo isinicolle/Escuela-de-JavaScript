55b477236bf9a2cd286292d54613aca13c615239
Isis Zapata <isis.zapata.hn@gmail.com>
2022-08-28 13:46:45 -0600

    .
    .

---
+++ Nuevo Curso Practico de JavaScript/script3.js
@@ -0,0 +1,7 @@
+
+const h1 = document.querySelector('h1')
+const button = document.querySelector('#calcular')
+
+const input1 = document.querySelector('#calculo1')
+const input2 = document.querySelector('#calculo2')
+

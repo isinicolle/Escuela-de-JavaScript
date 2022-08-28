3180ff89d2c836dd127369629ab6488af3151a62
Isis Zapata <isis.zapata.hn@gmail.com>
2022-08-28 14:25:04 -0600

    Update script3.js

--- Nuevo Curso Practico de JavaScript/script3.js
+++ Nuevo Curso Practico de JavaScript/script3.js
@@ -18,7 +18,7 @@ function btnOnclick(event){
 const h1 = document.querySelector('h1');
 const form = document.querySelector('#form'); 
 const input1 = document.querySelector('#calculo1');
-const input1 = document.querySelector('#calculo2');
+const input2 = document.querySelector('#calculo2');
 const btn = document.querySelector('#btnCalcular');
 const pResult = document.querySelector('#result');
 

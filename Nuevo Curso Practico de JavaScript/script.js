2147ad1bb10112a2ce19ac4857c4816e619f09eb
Isis Zapata <isis.zapata.hn@gmail.com>
2022-08-28 13:35:37 -0600

    .
    .

--- Nuevo Curso Practico de JavaScript/script.js
+++ Nuevo Curso Practico de JavaScript/script.js
@@ -4,3 +4,15 @@ const h1 = document.querySelector('h1')
 const p = document.querySelector('p')
 const parrafito = document.querySelector('.parrafito')
 const pid = document.getElementById('#pid')
+const input = document.querySelector('input')
+
+h1.innerHTML = 'Hola <br> mundo'
+p.innerText = 'Este es un parrafo'
+
+pid.getAttribute('pid')
+
+
+h1.setAttribute('class', 'rojo')
+
+h1.classList.add('verde')
+h1.classList.remove('verde')

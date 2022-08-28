dc6331e4d55a18892ca7771ab8336e6cefd8f533
Isis Zapata <isis.zapata.hn@gmail.com>
2022-08-28 13:58:18 -0600

    .
    .

--- Nuevo Curso Practico de JavaScript/script3.js
+++ Nuevo Curso Practico de JavaScript/script3.js
@@ -1,7 +1,13 @@
-
 const h1 = document.querySelector('h1')
 const button = document.querySelector('#calcular')
 
 const input1 = document.querySelector('#calculo1')
 const input2 = document.querySelector('#calculo2')
 
+
+function btnOnclick(){
+    var suma = Number(input1.value + input2.value)
+    h1.innerHTML = suma
+    return "La suma es: " + suma
+
+}

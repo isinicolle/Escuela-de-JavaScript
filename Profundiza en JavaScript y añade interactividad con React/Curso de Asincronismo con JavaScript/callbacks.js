16e155fb7eeb12e26b613c827362724897b06f12
Isis Zapata <isis.zapata.hn@gmail.com>
2022-08-29 22:29:01 -0600

    Update callbacks.js

--- Profundiza en JavaScript y añade interactividad con React/Curso de Asincronismo con JavaScript/callbacks.js
+++ Profundiza en JavaScript y añade interactividad con React/Curso de Asincronismo con JavaScript/callbacks.js
@@ -17,3 +17,24 @@ function sum(num1, num2) {
   }
   
   setTimeout(gretting, 2000, 'Oscar');
+
+
+  function sum(num1, num2) {
+    return num1 + num2;
+}
+
+function rest(num1, num2) {
+    return num1 - num2;
+}
+
+function mult(num1, num2) {
+    return num1 * num2;
+}
+
+function div(num1, num2) {
+    return num1 / num2;
+}
+
+function calc(num1, num2, callback) {
+    return callback(num1, num2);
+};

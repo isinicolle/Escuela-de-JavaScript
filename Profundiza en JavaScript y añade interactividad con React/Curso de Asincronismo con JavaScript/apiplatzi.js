8e59d95955ed2d2ca8982d9dd0c5df3bc4ce02f5
Isis Zapata <isis.zapata.hn@gmail.com>
2022-08-29 23:02:03 -0600

    Update apiplatzi.js

--- Profundiza en JavaScript y añade interactividad con React/Curso de Asincronismo con JavaScript/apiplatzi.js
+++ Profundiza en JavaScript y añade interactividad con React/Curso de Asincronismo con JavaScript/apiplatzi.js
@@ -17,3 +17,16 @@ function fetchData(urlApi, callback){ //urlApi: no confundir y colocar API
 	}
 	xhttp.send();
 }
+
+fetchData(`${API}/products`, function (error1, data1) {
+  if (error1) return console.error(error1);
+  fetchData(`${API}/products/${data1[0].id}`, function (error2, data2) {
+    if (error2) return console.error(error2);
+    fetchData(`${API}/categories/${data2?.category?.id}`, function (error3, data3) {
+      if (error3) return console.error(error3);
+      console.log(data1[0]);
+      console.log(data2.title);
+      console.log(data3.name);
+    });
+  });
+});

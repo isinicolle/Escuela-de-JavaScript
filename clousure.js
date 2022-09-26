7f63fa8eca03464611abb4e049161732838a93ce
isisflorentino <isis.zapata.hn@gmail.com>
2022-09-26 00:13:21 -0600

    Update clousure.js

--- clousure.js
+++ clousure.js
@@ -13,3 +13,9 @@ function alcancia(cantidadInicial){
   const otraAlcancia = alcancia(5)
   otraAlcancia(30) // 35
   otraAlcancia(20) // 55
+
+
+  const fruits = () => { if (true) { var fruit1 = 'apple'; const fruit2 = 'banana'; let fruit3 = 'kiwi'; } }
+  console.log(fruit1); // apple
+  console.log(fruit2); // ReferenceError: fruit2 is not defined
+  console.log(fruit3); // ReferenceError: fruit3 is not defined
